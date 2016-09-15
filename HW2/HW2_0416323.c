4e4cdd25c57582024cb44ff99e543122e87416c0
Taylor Huang <asd00012334@hotmail.com>
2016-09-16 03:37:49 +0800

    Update HW2_0416323.c

--- HW2/HW2_0416323.c
+++ HW2/HW2_0416323.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 int examine_p(int a){
-int factor,factor_sum;
-for(factor=1,factor_sum=0;factor<a;factor++)
-if(a%factor==0)
-factor_sum+=factor;
-return (a==factor_sum)?1:0;
+    int factor,factor_sum;
+    for(factor=1,factor_sum=0;factor<a;factor++)
+        if(a%factor==0)
+            factor_sum+=factor;
+    return (a==factor_sum)?1:0;
 }
 
 int examine_d(int a){
-int factor,factor_sum;
-for(factor=1,factor_sum=0;factor<a;factor++)
-if(a%factor==0)
-factor_sum+=factor;
-return (a>factor_sum)?1:0;
+    int factor,factor_sum;
+    for(factor=1,factor_sum=0;factor<a;factor++)
+        if(a%factor==0)
+            factor_sum+=factor;
+    return (a>factor_sum)?1:0;
 }
 
 
@@ -24,15 +24,12 @@ int main()
     printf("Please input N:");
     scanf("%d",&input);
     for(count=2;count<=input;count++)
-    {
         if(examine_p(count)==1)
-        perfect++;
+            perfect++;
         else if(examine_d(count)==1)
-        deficient++;
+            deficient++;
         else
-        abundant++;
-
-    }
+            abundant++;
     printf("DEFICIENT:%d\n",deficient);
     printf("PERFECT:%d\n",perfect);
     printf("ABUNDANT:%d\n",abundant);
