d36d44e46ed535b420b2512185f6201e88b2f8f3
Taylor Huang <asd00012334@hotmail.com>
2016-09-16 03:34:00 +0800

    Update with more elegant approach
    I just thought this code was quite ugly so I improved it :P

--- HW1/HW1_0416323.c
+++ HW1/HW1_0416323.c
@@ -3,26 +3,27 @@
 
 int main()
 {
-    int m,a,b,c,d,e;
-    Blank:
-    printf("Enter a 5-digit integer: ");
-    scanf("%d",&m);
-    a=m%10;
-    b=(m)%100/10;
-    c=(m)%1000/100;
-    d=(m)%10000/1000;
-    e=(m)/10000;
-    if(!(10000<=m&&m<100000))
-    {printf("Your input is not a 5-digit integer. Exit\n\n");
-    goto Blank;
+    int input;
+    while(1){
+        printf("Enter a 5-digit integer: ");
+        scanf("%d",&input)
+        if(!10000<=m&&m<100000)
+            printf("Your input is not a 5-digit integer. Exit\n\n");
+        else break;
     }
-    else if(a==e&&b==d)
-    printf("It is a palindrome.\n\n");
-    else
-    {printf("It is not a palindrome\n\n");
-    goto Blank;
+    int dig[5];
+    for(int cnt=0;cnt<5;cnt++){
+        dig[cnt]=m%10;
+        m/=10
     }
+    bool pali = true;
+    for(int cnt=0;cnt<2)
+        if(dig[cnt]!=dig[4-cnt]){
+            pali = false;
+            break;
+        }
+    if(pali) printf("It is a palindrome.\n\n");
+    else printf("It is not a palindrome\n\n");
     system("pause");
-
     return 0;
 }
