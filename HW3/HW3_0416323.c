fbf54fe0ff8b6dccf280ea43617ed09cd84e988f
Taylor Huang <asd00012334@hotmail.com>
2017-02-01 11:40:04 +0800

    Update HW3_0416323.c
    More Concise Version

--- HW3/HW3_0416323.c
+++ HW3/HW3_0416323.c
@@ -2,36 +2,22 @@
 #include <stdlib.h>
 
 char* fistType[3] = {"scissors","stone","paper"};
+char* msg[3]={"DRAW","YOU WIN","YOU LOSE"};
 
-int main()
-{
-    int fist,pc,round,win,lose,draw;
+int main(){
+    int fist,pc,round,status[3]={};
     srand(time(NULL));
     printf("Please input the round you want to play:");
     scanf("%d",&round);
-    win=lose=draw=0;
     while(round--){
         printf("#Please threw the Fist you want, 1=scissors 2=stone 3=paper: ");
         scanf("%d",&fist);
         pc=rand()%3;
-        printf("You threw %s, Computer threw %s, ",fistType[fist-1],fistType[pc]);
-        switch(fist-pc){
-            case 1:
-            printf("YOU WIN!!\n");
-            win++;
-            break;
-    
-            case -1:
-            printf("YOU LOSE!!\n");
-            lose++;
-            break;
-    
-            case 0:
-            printf("DRAW!!\n");
-            draw++;
-            break;
-        }
+        printf("You threw %s, Computer threw %s, ",fistType[--fist],fistType[pc]);
+        int game=(fist-pc+3)%3;
+        printf("%s!!\n",msg[game]);
+        ++status[game];
     }
-    printf("WIN: %d\nLOSE: %d\nDRAW: %d\n",win,lose,draw);
+    printf("WIN: %d\nLOSE: %d\nDRAW: %d\n",status[1],status[2],status[0]);
     return 0;
 }
